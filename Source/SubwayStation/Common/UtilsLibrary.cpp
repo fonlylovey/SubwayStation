2e78599d02763ed52c76cba119babde6eab5d08a
fengya <fonlylovey@163.com>
2023-03-24 16:52:13 +0800

    添加路径动画类和工具函数库

---
+++ Source/SubwayStation/Common/UtilsLibrary.cpp
@@ -0,0 +1,14 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UtilsLibrary.h"
+
+void UtilsLibrary::Log(FString str, bool bScreen, float fFtime, FColor color)
+{
+	str = FString::Printf(TEXT("Log: %s"), *str);
+	UE_LOG(LogTemp, Warning, TEXT("%s"), *str);
+	if (bScreen)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, fFtime, color, str);
+	}
+}
---
+++ Source/SubwayStation/Common/UtilsLibrary.h
@@ -0,0 +1,15 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class SUBWAYSTATION_API UtilsLibrary : UBlueprintFunctionLibrary
+{
+public:
+	UFUNCTION(BlueprintCallable, Category = "UtilsLibrary")
+	static void Log(FString str, bool bScreen = false, float fFtime = 3, FColor color = FColor::Green);
+};
